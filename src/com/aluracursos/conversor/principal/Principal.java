070c78a6ad1415fe2759a48106980931111d23dd
gtd <dvillarroela@pm.me>
2025-05-07 12:10:35 -0400

    Construir la Solicitud (HttpRequest)

--- src/com/aluracursos/conversor/principal/Principal.java
+++ src/com/aluracursos/conversor/principal/Principal.java
@@ -2,11 +2,15 @@ package com.aluracursos.conversor.principal;
 
 import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
 
 public class Principal {
 	public static void main(String[] args) {
 		URI direccion = URI.create("https://v6.exchangerate-api.com/v6/281a2b70643c0c8bae500d19/pair/EUR/GBP/7");
 
 		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder()
+				.uri(direccion)
+				.build();
 	}
 }
