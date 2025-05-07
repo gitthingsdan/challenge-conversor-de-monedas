292c45ebc5f4af0e1ac883eb68619114b68c7cff
gtd <dvillarroela@pm.me>
2025-05-07 12:06:43 -0400

    Construir el Cliente para Solicitudes (HttpClient)

--- src/com/aluracursos/conversor/principal/Principal.java
+++ src/com/aluracursos/conversor/principal/Principal.java
@@ -1,9 +1,12 @@
 package com.aluracursos.conversor.principal;
 
 import java.net.URI;
+import java.net.http.HttpClient;
 
 public class Principal {
 	public static void main(String[] args) {
 		URI direccion = URI.create("https://v6.exchangerate-api.com/v6/281a2b70643c0c8bae500d19/pair/EUR/GBP/7");
+
+		HttpClient client = HttpClient.newHttpClient();
 	}
 }
