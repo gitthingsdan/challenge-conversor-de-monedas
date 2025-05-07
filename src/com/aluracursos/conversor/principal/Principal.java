dfb3743b03cf20229524ea85c8c657e420e77660
gtd <dvillarroela@pm.me>
2025-05-07 12:11:22 -0400

    Construir la Respuesta (HttpResponse)

--- src/com/aluracursos/conversor/principal/Principal.java
+++ src/com/aluracursos/conversor/principal/Principal.java
@@ -1,16 +1,23 @@
 package com.aluracursos.conversor.principal;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class Principal {
-	public static void main(String[] args) {
-		URI direccion = URI.create("https://v6.exchangerate-api.com/v6/281a2b70643c0c8bae500d19/pair/EUR/GBP/7");
+	public static void main(String[] args) throws IOException, InterruptedException {
+		String apiKey = "281a2b70643c0c8bae500d19";
+		URI direccion = URI.create("https://v6.exchangerate-api.com/v6/" + apiKey + "/pair/EUR/GBP/7");
 
 		HttpClient client = HttpClient.newHttpClient();
 		HttpRequest request = HttpRequest.newBuilder()
 				.uri(direccion)
 				.build();
+		HttpResponse<String> response = client
+				.send(request, HttpResponse.BodyHandlers.ofString());
+
+		String json = response.body();
 	}
 }
