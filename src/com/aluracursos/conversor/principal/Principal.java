5474a1bdb3014897026646ab7b2959e2ca65ba0e
gtd <dvillarroela@pm.me>
2025-05-07 12:15:01 -0400

    Convertir la respuesta a objeto (Analizando la respuesta en formato JSON)

--- src/com/aluracursos/conversor/principal/Principal.java
+++ src/com/aluracursos/conversor/principal/Principal.java
@@ -1,5 +1,8 @@
 package com.aluracursos.conversor.principal;
 
+import com.aluracursos.conversor.modelos.Conversion;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -8,8 +11,7 @@ import java.net.http.HttpResponse;
 
 public class Principal {
 	public static void main(String[] args) throws IOException, InterruptedException {
-		String apiKey = "281a2b70643c0c8bae500d19";
-		URI direccion = URI.create("https://v6.exchangerate-api.com/v6/" + apiKey + "/pair/EUR/GBP/7");
+		URI direccion = URI.create("https://v6.exchangerate-api.com/v6/281a2b70643c0c8bae500d19/pair/EUR/GBP/7");
 
 		HttpClient client = HttpClient.newHttpClient();
 		HttpRequest request = HttpRequest.newBuilder()
@@ -17,7 +19,10 @@ public class Principal {
 				.build();
 		HttpResponse<String> response = client
 				.send(request, HttpResponse.BodyHandlers.ofString());
+		client.close();
 
 		String json = response.body();
+		ObjectMapper mapper = new ObjectMapper();
+		Conversion conversion = mapper.readValue(json, Conversion.class);
 	}
 }
