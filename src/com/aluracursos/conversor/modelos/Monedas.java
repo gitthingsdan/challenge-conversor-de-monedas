39072a74d4bdc0b2113239015246b92419d3a81a
gtd <dvillarroela@pm.me>
2025-05-09 15:09:02 -0400

    Añadir funciones de selección y conversión de divisas.
    Se ha introducido un menú interactivo para seleccionar las divisas de origen y destino, aprovechando una lista de divisas admitidas. Se ha mejorado la aplicación con indicaciones para la conversión de importes y llamadas dinámicas a la API basadas en la información introducida por el usuario, lo que aumenta su facilidad de uso y flexibilidad. Se ha actualizado el archivo .gitignore para excluir el archivo sensible ApiKey.java.

---
+++ src/com/aluracursos/conversor/modelos/Monedas.java
@@ -0,0 +1,8 @@
+package com.aluracursos.conversor.modelos;
+
+public enum Monedas {
+	USD, // Dólar
+	ARS, // Peso argentino
+	COP, // Peso colombiano
+	BRL // Real brasileño
+}
--- src/com/aluracursos/conversor/principal/Principal.java
+++ src/com/aluracursos/conversor/principal/Principal.java
@@ -1,6 +1,9 @@
 package com.aluracursos.conversor.principal;
 
 import com.aluracursos.conversor.modelos.Conversion;
+import com.aluracursos.conversor.modelos.Monedas;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
@@ -8,21 +11,68 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class Principal {
 	public static void main(String[] args) throws IOException, InterruptedException {
-		URI direccion = URI.create("https://v6.exchangerate-api.com/v6/281a2b70643c0c8bae500d19/pair/EUR/GBP/7");
+		ApiKey apiKey = new ApiKey();
+		Scanner sc = new Scanner(System.in);
 
-		HttpClient client = HttpClient.newHttpClient();
-		HttpRequest request = HttpRequest.newBuilder()
-				.uri(direccion)
-				.build();
-		HttpResponse<String> response = client
-				.send(request, HttpResponse.BodyHandlers.ofString());
-		client.close();
+		while (true) {
+//			Origen
+			String menuOrigen = """
+					****************************
+					¡Sea bienvenida/o al Conversor de Monedas!
 
-		String json = response.body();
-		ObjectMapper mapper = new ObjectMapper();
-		Conversion conversion = mapper.readValue(json, Conversion.class);
+					Elija el número correspondiente a la moneda de origen (o 0 para salir):
+					
+					1) Dólar (USD)
+					2) Peso argentino (ARS)
+					3) Peso colombiano (COP)
+					4) Real brasileño (BRL)
+					****************************
+					""";
+			System.out.println(menuOrigen);
+			int opcionOrigen = sc.nextInt();
+			if (opcionOrigen == 0) {
+				break;
+			}
+			Monedas origen = Monedas.values()[opcionOrigen - 1];
+
+//			Destino
+			String menuDestino = """
+					****************************
+					Elija el número correspondiente a la moneda de destino (o 0 para salir):
+					
+					1) Dólar (USD)
+					2) Peso argentino (ARS)
+					3) Peso colombiano (COP)
+					4) Real brasileño (BRL)
+					****************************
+					""";
+			System.out.println(menuDestino);
+			int opcionDestino = sc.nextInt();
+			if (opcionDestino == 0) {
+				break;
+			}
+			Monedas destino = Monedas.values()[opcionDestino - 1];
+
+			System.out.println("Ingrese la cantidad a convertir (formato: 999,99...): ");
+			double cantidad = sc.nextDouble();
+
+			URI direccion = URI.create("https://v6.exchangerate-api.com/v6/" + apiKey.getApiKey() + "/pair/" + origen + "/" + destino + "/" + cantidad);
+
+			HttpClient client = HttpClient.newHttpClient();
+			HttpRequest request = HttpRequest.newBuilder().uri(direccion).build();
+			HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+			client.close();
+
+			ObjectMapper mapper = new ObjectMapper();
+			mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+			mapper.enable(JsonParser.Feature.INCLUDE_SOURCE_IN_LOCATION);
+			Conversion conversion = mapper.readValue(response.body(), Conversion.class);
+
+			System.out.println("El valor " + cantidad + " ["+origen+"] corresponde al valor final de => " + conversion.conversion_result() + " ["+destino+"]");
+		}
 	}
 }
